fead49062e22181e3b1ec342afdc84ec9b984064
liyunlai <liyunlai@jingxunsoft.com>
2024-02-20 10:54:35 +0800

    update script

--- kitsunebi/genRules.sh
+++ kitsunebi/genRules.sh
@@ -100,7 +100,90 @@ do_work_test() {
 
 do_work_kitsunebi() {
     local FN="$1"
-    echo "[Rule]" >$FN
+
+    echo "# $(date)" >$FN
+
+    cat >>$FN <<EOF
+[Rule]
+# LAN
+GEOIP,private,DIRECT
+
+# DNS
+IP-CIDR,119.29.29.29/32,DIRECT
+IP-CIDR,223.5.5.5/32,DIRECT
+IP-CIDR,1.1.1.1/32,PROXY
+IP-CIDR,1.0.0.1/32,PROXY
+IP-CIDR,8.8.8.8/32,PROXY
+IP-CIDR,8.8.4.4/32,PROXY
+IP-CIDR,9.9.9.9/32,PROXY
+IP-CIDR,149.112.112.112/32,PROXY
+IP-CIDR,208.67.222.222/32,PROXY
+IP-CIDR,208.67.220.220/32,PROXY
+
+// LINE
+IP-CIDR,125.209.222.0/22,Proxy,no-resolve
+IP-CIDR,203.104.128.0/20,Proxy,no-resolve
+
+// Telegram
+IP-CIDR,149.154.167.0/22,Proxy,no-resolve
+IP-CIDR,149.154.175.0/22,Proxy,no-resolve
+IP-CIDR,91.108.56.0/22,Proxy,no-resolve
+
+GEOIP,cn,DIRECT
+DOMAIN-KEYWORD,geosite:cn,DIRECT
+
+USER-AGENT,wechat,DIRECT
+
+USER-AGENT,twitter,PROXY
+DOMAIN-KEYWORD,twitter,PROXY
+
+USER-AGENT,gmail,PROXY
+DOMAIN-KEYWORD,gmail,PROXY
+
+USER-AGENT,telegram,PROXY
+DOMAIN-KEYWORD,telegram,PROXY
+
+USER-AGENT,tumblr,PROXY
+DOMAIN-KEYWORD,tumblr,PROXY
+
+USER-AGENT,facebook,PROXY
+DOMAIN-KEYWORD,facebook,PROXY
+
+USER-AGENT,pinterest,PROXY
+DOMAIN-KEYWORD,pinterest,PROXY
+
+USER-AGENT,instagram,PROXY
+DOMAIN-KEYWORD,instagram,PROXY
+
+# 
+DOMAIN-KEYWORD,google,PROXY
+DOMAIN-KEYWORD,youtube,PROXY
+DOMAIN-KEYWORD,github,PROXY
+DOMAIN-KEYWORD,gitlab,PROXY
+DOMAIN-KEYWORD,yahoo,Proxy,force-remote-dns
+DOMAIN-KEYWORD,wikipedia,Proxy,force-remote-dns
+DOMAIN-KEYWORD,telegram,Proxy,force-remote-dns
+DOMAIN-KEYWORD,whatsapp,Proxy,force-remote-dns
+DOMAIN-KEYWORD,line,Proxy,force-remote-dns
+DOMAIN-KEYWORD,blogspot,Proxy,force-remote-dns
+DOMAIN-KEYWORD,shadowsocks,Proxy,force-remote-dns
+DOMAIN-KEYWORD,skype,Proxy,force-remote-dns
+DOMAIN-KEYWORD,bing,Proxy,force-remote-dns
+DOMAIN-KEYWORD,netflix,Proxy,force-remote-dns
+DOMAIN-KEYWORD,snapchat,Proxy,force-remote-dns
+DOMAIN-KEYWORD,openai,Proxy,force-remote-dns
+DOMAIN-SUFFIX,character.ai,Proxy,force-remote-dns
+DOMAIN-SUFFIX,v2ex.com,Proxy,force-remote-dns
+DOMAIN-KEYWORD,dropbox,Proxy,force-remote-dns
+DOMAIN-KEYWORD,quora,Proxy,force-remote-dns
+DOMAIN-KEYWORD,reddit,Proxy,force-remote-dns
+DOMAIN-KEYWORD,wired,Proxy,force-remote-dns
+DOMAIN-KEYWORD,bloomberg,Proxy,force-remote-dns
+DOMAIN-KEYWORD,stackoverflow,Proxy,force-remote-dns
+DOMAIN-KEYWORD,stackexchange,Proxy,force-remote-dns
+
+EOF
+
     local tmpfile=$(mktemp)
     do_work gen_kitsunebi_rule >>${tmpfile}
     cat $tmpfile | grep -v '[=%]' | awk '!(NF && seen[$0]++)' >>$FN
@@ -114,7 +197,21 @@ do_work_kitsunebi() {
 223.5.5.5
 119.29.29.29
 8.8.8.8,53,REMOTE
-8.8.4.4,53,REMOTE
+8.8.4.4
+
+[DnsHost]
+# Static DNS map that functions in the same way as /etc/hosts.
+localhost=127.0.0.1
+www.localnetwork.uop=127.0.0.1
+
+[DnsClientIp]
+# Client IP for EDNS Client Subnet extension, a single IP address.
+115.239.211.92
+
+[RoutingDomainStrategy]
+# https://www.v2ray.com/chapter_02/03_routing.html
+IPIfNonMatch
+
 EOF
 
 }
